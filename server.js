79f006565670fab33ed24581baf3326cc6ccef19
mc4tech <chavezmeguel@yahoo.com>
2017-08-08 23:32:14 -0400

    update

--- server.js
+++ server.js
@@ -16,8 +16,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-require("./app/routing/apiRoutes.js")(app);
-require("./app/routing/htmlRoutes.js")(app);
 
 // Starts the server to begin listening
 // =============================================================
