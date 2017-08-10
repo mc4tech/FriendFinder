e05617eb8970866c61816e9ef69fd82f8fc6aa20
mc4tech <chavezmeguel@yahoo.com>
2017-08-10 18:24:05 -0400

    added public js

---
+++ app/public/js/questions.js
@@ -0,0 +1,32 @@
+questions = [
+	{
+		"question": "You prefer frontend."
+	},
+	{
+		"question": "You prefer backend."
+	},
+	{
+		"question": "You are a javascript pro."
+	},
+	{
+		"question": "You prefer your own CSS compared to bootstrap."
+	},
+	{
+		"question": "Your creativity is evident in your CSS."
+	},
+	{
+		"question": "Generally you find it easy to start coding."
+	},
+	{
+		"question": "You keep your cool under pressure."
+	},
+	{
+		"question": "When it comes to backend, you're in beastnode."
+	},
+	{
+		"question": "It's easy to keep focus."
+	},
+	{
+		"question": "You get lost in the broad scope."
+	}
+]
--- server.js
+++ server.js
@@ -3,12 +3,16 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
-
+var htmlRoutes = require("./app/routing/htmlRoutes.js");
+var apiRoutes = require("./app/routing/apiRoutes.js");
+// var friendsJS = require("./app/data/friends.js");
 
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 8080;
+// var PORT = process.env.PORT || 8080;
+var PORT = 3000;
+
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
@@ -16,6 +20,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
+app.use('/public', express.static(path.join(__dirname, 'app/public')))
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, "./app/public/home.html"));
+});
+
+app.get('/survey', function(req, res) {
+    res.sendFile(path.join(__dirname, "./app/public/survey.html"));
+});
 
 // Starts the server to begin listening
 // =============================================================
